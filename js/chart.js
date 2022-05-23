873a2ee962372dabab88c479be886b52b1593b24
Jorge <jorgesuarez9204@gmail.com>
2022-05-22 22:27:35 -0500

    add scss

--- js/chart.js
+++ js/chart.js
@@ -1,4 +1,36 @@
 const trafficChart = document.getElementById("traffic-chart");
+const alertBanner = document.getElementById("alert");
+const user = document.getElementById("userField");
+const message = document.getElementById("messageField");
+const send = document.getElementById("send");
+
+alertBanner.innerHTML = `<div class="alert-banner"><p>
+<strong>Alert:</strong> You have <strong>6</strong> overdue tasks to
+      complete
+    </p>
+    <p class="alert-banner-close">x</p>
+  </div>`;
+
+alertBanner.addEventListener("click", (e) => {
+  const element = e.target;
+  if (element.classList.contains("alert-banner-close")) {
+    alertBanner.style.display = "none";
+  }
+});
+
+send.addEventListener("click", () => {
+  // ensure user and message fields are filled out
+  if (user.value === "" && message.value === "") {
+    alert("Please fill out user and message fields before sending");
+  } else if (user.value === "") {
+    alert("Please fill out user field before sending");
+  } else if (message.value === "") {
+    alert("Please fill out message field before sending");
+  } else {
+    alert(`Message successfully sent to: ${user.value}`);
+  }
+});
+
 let myChart = new Chart(trafficChart, {
   type: "line",
   data: {
